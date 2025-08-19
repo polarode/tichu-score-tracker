77519c19bb0eeaeddcdc0e741521d2409234b58d
Marcel Huber <git@marcelhuber.de>
2025-08-20 00:17:11 +0200

    Tab stored as query param, reorder

--- src/pages/tichu/statistics/index.tsx
+++ src/pages/tichu/statistics/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Tabs, Tab, Box } from "@mui/material";
 import { PageTemplate } from "../../../components/PageTemplate";
 import { Trans } from "@lingui/react/macro";
@@ -17,30 +18,42 @@ const TabPanel = ({ children, value, index }: TabPanelProps) => (
 );
 
 export const Statistics = () => {
-    const [tabValue, setTabValue] = useState(0);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [tabValue, setTabValue] = useState(() => {
+        const tab = searchParams.get('tab');
+        return tab === 'players' ? 1 : tab === 'teams' ? 2 : 0;
+    });
+
+    useEffect(() => {
+        const tab = searchParams.get('tab');
+        const newValue = tab === 'players' ? 1 : tab === 'teams' ? 2 : 0;
+        setTabValue(newValue);
+    }, [searchParams]);
 
     const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
         setTabValue(newValue);
+        const tabName = newValue === 1 ? 'players' : newValue === 2 ? 'teams' : 'overview';
+        setSearchParams({ tab: tabName });
     };
 
     return (
         <PageTemplate maxWidth="lg" showVersionButton={false}>
             <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                 <Tabs value={tabValue} onChange={handleTabChange}>
+                    <Tab label={<Trans>Game Overview</Trans>} />
                     <Tab label={<Trans>Players</Trans>} />
                     <Tab label={<Trans>Teams</Trans>} />
-                    <Tab label={<Trans>Game Overview</Trans>} />
                 </Tabs>
             </Box>
 
             <TabPanel value={tabValue} index={0}>
-                <PlayerRanking />
+                <GameOverview />
             </TabPanel>
             <TabPanel value={tabValue} index={1}>
-                <TeamRanking />
+                <PlayerRanking />
             </TabPanel>
             <TabPanel value={tabValue} index={2}>
-                <GameOverview />
+                <TeamRanking />
             </TabPanel>
         </PageTemplate>
     );
